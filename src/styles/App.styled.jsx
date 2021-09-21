c33cc05942f236da00fe6286dee7727e05237024
Sasha Havrylov <sashagavrilov1995@gmail.com>
2021-09-21 21:18:43 +0300

    react create app

---
+++ src/styles/App.styled.jsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  padding: 24px;
+  align-items: center;
+  text-align: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 36px;
+`;
+
+export const Section = styled.div`
+  padding: 24px;
+`;
